b35be499b41a1315f7a2f1f3d620da9f40235bab
tam <cherykim123@gmail.com>
2020-10-08 16:52:37 -0700

    componentWillUnmount

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -6,9 +6,16 @@ function Counter(props) {
   const [name, setName] = useState("");
   //add input state variable, .target.value is for updating state variable
 
+  //componentDidMount()
+  //componentUpdate()
+  //componentWillUnmount() for where we write clean up code
   useEffect(() => {
     document.title = `${name} has clicked ${count} time`;
-  });
+    return () => {
+      //whenever we increase or change the name, we see clean up message
+      console.log("clean up ");
+    };
+  }, []); // pass a dependency array to preventing clean up message everytime we click
   return (
     <Fragment>
       <input type="text" onChange={(e) => setName(e.target.value)}></input>
