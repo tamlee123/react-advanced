3ed6ffce9349f9c08c697b29abe13f0bfdfb6563
tam <cherykim123@gmail.com>
2020-10-08 16:01:00 -0700

    declaring multible state variables- adding input

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -2,9 +2,12 @@ import React, { useState, Fragment } from "react";
 
 function Counter(props) {
   const [count, setState] = useState(0);
+  const [name, setName] = useState("");
 
   return (
     <Fragment>
+      //add input state variable, .target.value is for updating state variable
+      <input type="text" onChange={(e) => setName(e.target.value)}></input>
       <div>counter: {count}</div>
       <button onClick={() => setState(count + 1)}>Increase</button>
     </Fragment>
