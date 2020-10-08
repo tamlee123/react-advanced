33f5483328d12c8a2a3ee7cdce30395b772d47d5
tam <cherykim123@gmail.com>
2020-10-08 16:36:23 -0700

    8) the useEffect hook. set document.title

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -1,9 +1,14 @@
 import React, { useState, Fragment } from "react";
+import { useEffect } from "react";
 
 function Counter(props) {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
   //add input state variable, .target.value is for updating state variable
+
+  useEffect(() => {
+    document.title = `${name} has clicked ${count} time`;
+  });
   return (
     <Fragment>
       <input type="text" onChange={(e) => setName(e.target.value)}></input>
