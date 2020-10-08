a404d89a0d262f33e0cf8d6f7a6ad6c7a4f72437
tam <cherykim123@gmail.com>
2020-10-08 16:19:33 -0700

    changing set

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -1,17 +1,16 @@
 import React, { useState, Fragment } from "react";
 
 function Counter(props) {
-  const [count, setState] = useState(0);
+  const [count, setCount] = useState(0);
   const [name, setName] = useState("");
-
+  //add input state variable, .target.value is for updating state variable
   return (
     <Fragment>
-      //add input state variable, .target.value is for updating state variable
       <input type="text" onChange={(e) => setName(e.target.value)}></input>
       <div>
         {name} has clicked {count} times!
       </div>
-      <button onClick={() => setState(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
     </Fragment>
   );
 }
