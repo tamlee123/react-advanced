d729ef39f85db61a5cc845658472da37396814bb
tam <cherykim123@gmail.com>
2020-10-08 15:26:29 -0700

    7) the useState hooks

---
+++ src/hooks/Counter.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function Counter(props) {
+  const array = useState(0);
+  const count = array[0]; // this.state.count
+  const setState = array[1]; //this.setState()
+  return <div>counter: {}</div>;
+}
+
+export default Counter;
